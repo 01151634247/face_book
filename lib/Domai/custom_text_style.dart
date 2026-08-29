815e874b888caf2d8ce95e59acf4f3bdd4e08152
ayman <aymanmohamed1142001@gmail.com>
2026-08-30 00:52:26 +0300

    create DefaultTabController

---
+++ lib/Domai/custom_text_style.dart
@@ -0,0 +1,64 @@
+
+import 'package:flutter/material.dart';
+
+   class text{
+
+  static TextStyle textStyle15 =TextStyle(
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+    fontFamily: 'primary',
+    fontSize: 15,
+  );
+
+  static TextStyle textStyle18 =TextStyle(
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+    fontFamily: 'primary',
+    fontSize: 18,
+  );
+
+
+  static TextStyle textStyle20 =TextStyle(
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+    fontFamily: 'primary',
+    fontSize: 20,
+  );
+
+ static  TextStyle textStyle22 =TextStyle(
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+    fontFamily: 'primary',
+    fontSize: 22,
+  );
+
+
+ static TextStyle textStyle25 =TextStyle(
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+    fontFamily: 'primary',
+    fontSize: 25,
+  );
+
+
+static TextStyle textStyle28 =TextStyle(
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+    fontFamily: 'primary',
+    fontSize: 28,
+  );
+
+  static TextStyle textStyle30 =TextStyle(
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+    fontFamily: 'primary',
+    fontSize: 30,
+
+
+  );
+
+
+
+
+
+}
---
+++ lib/screens/dash_bord_screen.dart
@@ -0,0 +1,80 @@
+import 'package:face_book/Domai/custom_text_style.dart';
+import 'package:flutter/material.dart';
+
+class DashBordScreen extends StatefulWidget {
+  const DashBordScreen({super.key});
+
+  @override
+  State<DashBordScreen> createState() => _DashBordScreenState();
+}
+
+class _DashBordScreenState extends State<DashBordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 6,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'faceBook',
+              style: text.textStyle30.copyWith(
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            actions: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Icon(
+                  Icons.add_sharp,
+                  color: Colors.white,
+                  size: 28,
+                ),
+              ),
+              const SizedBox(width: 5),
+              const Icon(Icons.search_rounded, color: Colors.black, size: 40),
+              const SizedBox(width: 5),
+              Image.asset('assets/icons/chat.png', height: 30),
+              const SizedBox(width: 7),
+            ],
+            bottom: TabBar(
+              indicatorColor: Colors.blue,
+              automaticIndicatorColorAdjustment: true,
+              labelColor: Colors.blueAccent,
+              tabs: [
+                
+                const Tab(icon: Icon(Icons.home)),
+                const Tab(icon: Icon(Icons.video_collection)),
+                const Tab(icon: Icon(Icons.group)),
+                const Tab(icon: Icon(Icons.storefront)),
+                const Tab(icon: Icon(Icons.notifications)),
+                Tab(
+                  icon: ClipOval(
+                    child: Image.asset(
+                      'assets/images/girls3.jpg',
+                      height: 28,
+                      width: 28,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+
+          body: TabBarView(
+            children: 
+            [
+              
+            ]
+            ),
+        ),
+      ),
+    );
+  }
+}
