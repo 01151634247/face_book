b27d3b02cdadbb0a6fff0005e5ddc53d63abea58
ayman <aymanmohamed1142001@gmail.com>
2026-08-30 01:57:38 +0300

    create story card in home screen

--- lib/screens/tabs/home_screen.dart
+++ lib/screens/tabs/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:face_book/Domai/custom_text_style.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,7 +12,90 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('HomeScreen'),),
+      body:Column(children: [
+        SizedBox(
+          height:15 ,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children:
+           [
+            ClipOval(child: Image.asset('assets/images/pexels-dana-tentis-118658-364382.jpg',height: 50,width: 50,fit:BoxFit.cover,)),
+              SizedBox(
+                width: 260,
+                height: 50,
+                child: TextField(
+                  
+                  decoration: InputDecoration(
+                    hintText: 'what is in your mind?',
+                    hintStyle: text.textStyle15,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:const BorderSide(color: Colors.black54,width: 1),
+                     borderRadius: BorderRadius.circular(20),),
+                     disabledBorder: OutlineInputBorder(
+                       borderSide:const BorderSide(color: Colors.black54,width: 1),
+                     borderRadius: BorderRadius.circular(20),),
+                     enabledBorder: OutlineInputBorder(
+                       borderSide:const BorderSide(color: Colors.black54,width: 1),
+                     borderRadius: BorderRadius.circular(20),),
+                
+                    ),
+                  ),
+              ),
+
+              Icon(Icons.image,size: 47,color: Colors.green,)
+        ],
+        ),
+          Divider(
+            thickness: 3,
+          ),
+         SizedBox(
+          height: 200,
+           child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              SizedBox(
+                height: 200,
+                width: 120,
+                child: Card(
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(topLeft: Radius.circular(12),topRight: Radius.circular(12),),
+                          image: DecorationImage(image: AssetImage('assets/images/pexels-ira-dulger-647031-1452129.jpg'),fit:BoxFit.cover),
+                        ),
+                        height: 100,
+                        width: 120,
+                       ),
+                      Positioned(
+                        top: 85,
+                        left:38 ,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius: BorderRadius.circular(100),
+                            border: Border.all(
+                              color: Colors.white,width: 2,
+                            )
+                          ),
+                          child: Icon(Icons.add,size: 35,color: Colors.white,),
+                        ),
+                      ),
+                        Positioned(
+                          bottom: 4,
+                          left: 12,
+                          child: Text('Create Story',style: text.textStyle15,)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+           
+           ),
+         ),
+     
+      ],),
     );
   }
 }
