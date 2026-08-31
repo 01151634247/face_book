cb22405f54b1f9ee2af6b91e54784a59804dd73c
ayman <aymanmohamed1142001@gmail.com>
2026-08-31 04:18:13 +0300

    create user profile and icon in notification card

--- lib/widgets/notification_card.dart
+++ lib/widgets/notification_card.dart
@@ -1,3 +1,4 @@
+import 'package:face_book/Domai/custom_text_style.dart';
 import 'package:flutter/material.dart';
 
 class NotificationCard extends StatelessWidget {
@@ -10,9 +11,20 @@ class NotificationCard extends StatelessWidget {
        const SizedBox(
           height: 100,
         ),
-        ClipOval(child: Image.asset('assets/images/pexels-olenagoldman-1021693.jpg',height:80 ,width: 80,fit: BoxFit.cover,)),
-      
-    ],
+         SizedBox(
+          height: 120,
+           child: Stack(
+            
+             children: [
+               ClipOval(child: Image.asset('assets/images/pexels-olenagoldman-1021693.jpg',height:80 ,width: 80,fit: BoxFit.cover,),),
+              Positioned(
+                bottom: 37,
+                left: 55,
+                child:Image.asset('assets/icons/chat (1).png',height: 25,width: 25,) ,),
+             ],
+           ),
+         ),
+      ]
     );
   }
 }
